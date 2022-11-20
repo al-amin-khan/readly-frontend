d0cd4310303d30a870304966f3f2533bc1eab4e4
Al Amin Khan <al.amin@bracits.com>
2022-11-20 18:41:48 +0600

    add card view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { HttpClientModule } from '@angular/common/http'
+import { BooksService } from './books/books.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
   ],
-  providers: [],
+  providers: [BooksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
